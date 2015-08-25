5d3d4fd451f5eeaeef9c3caa01a6b1aa30e968a1
Mitchell Bank <mitchbank26@gmail.com>
2015-08-25 16:38:12 -0400

    Added contact class

--- crm.rb
+++ crm.rb
@@ -1,5 +1,15 @@
+require_relative 'contact.rb'
 require 'sinatra'
 
 get '/' do
-	rb :index
+	@crm_app_name = "My CRM"
+	erb :index
+end
+
+get '/contacts' do
+	erb :contacts
+end
+
+get '/contacts/new' do
+	"Create and new contact"
 end
---
+++ views/contact.rb
@@ -0,0 +1,19 @@
+class Contact
+	attr_accessor :id, :first_name, :last_name, :email, :notes
+
+	def initialize(id, first_name, last_name, email, notes)
+		@id = id
+		@first_name = first_name
+		@last_name = last_name
+		@email = email
+		@notes = notes
+	end
+
+	def full_name
+		first_name + " " + last_name		
+	end
+
+	def modify(contact)
+		
+	end
+end
