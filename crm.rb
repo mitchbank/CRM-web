32494728a05258c3b3da0a05f8880f48bff08cc6
Mitchell Bank <mitchbank26@gmail.com>
2015-08-25 18:15:50 -0400

    Added new_contact erb

--- crm.rb
+++ crm.rb
@@ -2,17 +2,14 @@ require_relative 'rolodex'
 require_relative 'contact'
 require 'sinatra'
 
+$rolodex = Rolodex.new
+
 get '/' do
 	@crm_app_name = "My CRM"
 	erb :index
 end
 
 get '/contacts' do
-	@contacts = []
-	@contacts << Contact.new("Yehuda", "Katz", "yehuda@example.com", "Developer")
-	@contacts << Contact.new("Mark", "Zuckerberg", "Mark@facebook.com", "CEO")
-	@contacts << Contact.new("Sergei", "Brin", "Sergei@google.com", "Used to be CEO")
-
 	erb :contacts
 end
 
