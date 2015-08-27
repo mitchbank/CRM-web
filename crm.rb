bf61da8c623d77b688fc8112144545e23112ec8c
Mitchell Bank <mitchbank26@gmail.com>
2015-08-27 16:48:32 -0400

    Can now edit any contact

--- crm.rb
+++ crm.rb
@@ -39,6 +39,19 @@ get '/contacts/:id/edit' do
 		erb :edit_contact
 	else
 		raise Sinatra::NotFound
-		redirect to('/contacts')
 	end
 end
+
+put '/contact/:id' do 
+		@contact = $rolodex.find(params[:id].to_i)
+	if @contact
+		@contact.first_name = params[:first_name]
+		@contact.last_name = params[:last_name]
+		@contact.email = params[:email]
+		@contact.notes = params[:notes]
+
+		redirect to('/contacts')
+	else
+		raise Sinatra::NotFound
+	end
+end	
